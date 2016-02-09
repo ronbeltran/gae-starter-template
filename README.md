c12a350e684e18f779236469e9c1ed678e612a91
Ronnie Beltran <rbbeltran.09@gmail.com>
2016-02-09 14:38:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## GAE Starter project template using Flask on Google App Engine
+
+
+### To install the needed libraries:
+    
+    pip install -t lib -r requirements.txt
+
+### To run:
+
+    dev_appserver.py app.yaml
---
+++ appengine_config.py
@@ -0,0 +1,8 @@
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
+
+
+from google.appengine.ext import vendor
+vendor.add('lib')
